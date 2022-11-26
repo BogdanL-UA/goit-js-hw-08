e29319d1e37f0b322a27da29c4ced2a6ded6cd2e
Bogdan Lytvynyuk <103260029+BogdanL-UA@users.noreply.github.com>
2022-11-26 23:43:08 +0200

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  try {
+    return localStorage.setItem('videoplayer-current-time', seconds);
+  } catch (error) {
+    console.log(error.name);
+    return [];
+  }
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input'),
+  message: document.querySelector('textarea'),
+  submit: document.querySelector('button'),
+};
+const FORM_KEY_LS = 'feedback-form-state';
+const formData = {};
+
+const onSubmitClick = event => {
+  event.preventDefault();
+  if (refs.email.value === '') {
+    alert('All filds of the form must be filled!');
+  } else if (refs.message.value === '') {
+    alert('All filds of the form must be filled!');
+  } else {
+    localStorage.removeItem(FORM_KEY_LS);
+    console.log(formData);
+    refs.form.reset();
+  }
+};
+
+const onInputSetValueInLS = event => {
+  if (event.target.value.trim() !== '') {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(FORM_KEY_LS, JSON.stringify(formData));
+  }
+};
+
+const checkLS = parsedFormData => {
+  if (parsedFormData === null) {
+    return;
+  }
+  if (parsedFormData.email !== '') {
+    refs.email.value = parsedFormData.email;
+  }
+  if (parsedFormData.message !== '') {
+    refs.message.value = parsedFormData.message;
+  }
+};
+
+getFormDataFromLS();
+
+function getFormDataFromLS() {
+  const sevedFormData = localStorage.getItem(FORM_KEY_LS);
+  const parsedFormData = JSON.parse(sevedFormData);
+  checkLS(parsedFormData);
+}
+
+refs.form.addEventListener('input', throttle(onInputSetValueInLS, 500));
+refs.submit.addEventListener('click', onSubmitClick);
